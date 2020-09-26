713747245d3b8ee5e4ce90aaa276a8c38c60870c
Anik Sen <anik.is.anifriend@gmail.com>
2020-09-26 23:45:18 +0530

    Fixed filter issue. Implemented sorting.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,13 @@
   <section class="w3l-albums py-5" id="projects">
     <div class="container py-lg-4">
       <fieldset class="border p-2">
-        <legend class="w-auto">Filter by</legend>
+        <legend class="w-auto">Sort &amp; Filter</legend>
         <div class="row" style="padding: 0 0 25px 0;">
           <div class="col-lg-3 mt-lg-0 mt-0">
             <label style="font-weight: bold;">Language</label>
             <select
               class="form-control"
-              @change.prevent="filterByLanguage($event)"
+              @change.prevent="filterByLanguageEvent($event)"
             >
               <option value="0">ALL</option>
               <option
@@ -23,13 +23,30 @@
             <label style="font-weight: bold;">Location</label>
             <select
               class="form-control"
-              @change.prevent="filterByLocation($event)"
+              @change.prevent="filterByLocationEvent($event)"
             >
               <option value="0">ALL</option>
               <option v-for="loc in locations" :key="loc.id" :value="loc.id">{{
                 loc.locationName
               }}</option>
             </select>
+          </div>
+          <div class="col-lg-5 mt-lg-0 mt-0">
+            <div class="pull-right">
+              <label style="font-weight: bold;">&nbsp;</label>
+              <h4>
+                <a
+                  class="show-title"
+                  :title="'Sort by ' + sort.title"
+                  href="javascript:void(0);"
+                  @click.prevent="sortListEvent"
+                  ><i
+                    :class="'fa fa-sort-alpha-' + sort.icon"
+                    aria-hidden="true"
+                  ></i>
+                </a>
+              </h4>
+            </div>
           </div>
         </div>
       </fieldset>
@@ -237,7 +254,14 @@ export default {
           id: 0,
           locationName: ""
         }
-      ]
+      ],
+      ddlLocationVal: 0,
+      ddlLanguageVal: 0,
+      sort: {
+        sortby: "ASC",
+        icon: "asc",
+        title: "A to Z"
+      }
     };
   },
   created() {
@@ -288,8 +312,9 @@ export default {
             _upcomingMovies.push(resp.data[i]);
           }
         }
-        _vm.$store.state.nowshowing = _vm.nowShowingMovies = _nowShowingMovies;
-        _vm.$store.state.upcoming = _vm.upcomingMovies = _upcomingMovies;
+        _vm.$store.state.nowshowing = _vm.nowShowingMovies = _vm.tempNowShowing = _nowShowingMovies;
+        _vm.$store.state.upcoming = _vm.upcomingMovies = _vm.tempUpcoming = _upcomingMovies;
+        _vm.sortData();
       });
     },
     getLanguages() {
@@ -306,37 +331,122 @@ export default {
         _vm.locations = resp.data;
       });
     },
-    filterByLanguage(event) {
-      const _vm = this;
-      if (event.target.value * 1 !== 0) {
-        _vm.nowShowingMovies = _vm.$func.searchByLanguageId(
-          event.target.value,
-          _vm.$store.state.nowshowing
+    filterByLanguage(vm, value, nowShowingList, upcomingList) {
+      if (value !== 0) {
+        vm.nowShowingMovies = vm.$func.searchByLanguageId(
+          value,
+          nowShowingList
         );
-        _vm.upcomingMovies = _vm.$func.searchByLanguageId(
-          event.target.value,
-          _vm.$store.state.upcoming
+        vm.upcomingMovies = vm.$func.searchByLanguageId(value, upcomingList);
+        vm.filterByLocation(
+          vm,
+          vm.ddlLocationVal,
+          vm.nowShowingMovies,
+          vm.upcomingMovies
         );
       } else {
-        _vm.nowShowingMovies = _vm.$store.state.nowshowing;
-        _vm.upcomingMovies = _vm.$store.state.upcoming;
+        if (vm.ddlLocationVal === 0) {
+          vm.nowShowingMovies = vm.$store.state.nowshowing;
+          vm.upcomingMovies = vm.$store.state.upcoming;
+        } else {
+          vm.filterByLocation(
+            vm,
+            vm.ddlLocationVal,
+            vm.nowShowingMovies,
+            vm.upcomingMovies
+          );
+        }
+      }
+      vm.sortData();
+    },
+    filterByLocation(vm, value, nowShowingList, upcomingList) {
+      if (value !== 0) {
+        vm.nowShowingMovies = vm.$func.searchByLocationId(
+          value,
+          nowShowingList
+        );
+        vm.upcomingMovies = vm.$func.searchByLocationId(value, upcomingList);
+        vm.filterByLanguage(
+          vm,
+          vm.ddlLanguageVal,
+          vm.nowShowingMovies,
+          vm.upcomingMovies
+        );
+      } else {
+        if (vm.ddlLanguageVal === 0) {
+          vm.nowShowingMovies = vm.$store.state.nowshowing;
+          vm.upcomingMovies = vm.$store.state.upcoming;
+        } else {
+          vm.filterByLanguage(
+            vm,
+            vm.ddlLanguageVal,
+            vm.nowShowingMovies,
+            vm.upcomingMovies
+          );
+        }
+      }
+      vm.sortData();
+    },
+    sortData() {
+      const _vm = this;
+      if (_vm.sort.sortby === "ASC") {
+        _vm.nowShowingMovies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? 1 : b.movieTitle > a.movieTitle ? -1 : 0
+        );
+        _vm.upcomingMovies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? 1 : b.movieTitle > a.movieTitle ? -1 : 0
+        );
+        _vm.movies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? 1 : b.movieTitle > a.movieTitle ? -1 : 0
+        );
+      } else {
+        _vm.nowShowingMovies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? -1 : b.movieTitle > a.movieTitle ? 1 : 0
+        );
+        _vm.upcomingMovies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? -1 : b.movieTitle > a.movieTitle ? 1 : 0
+        );
+        _vm.movies.sort((a, b) =>
+          a.movieTitle > b.movieTitle ? -1 : b.movieTitle > a.movieTitle ? 1 : 0
+        );
       }
     },
-    filterByLocation(event) {
+    filterByLanguageEvent(event) {
       const _vm = this;
-      if (event.target.value * 1 !== 0) {
-        _vm.nowShowingMovies = _vm.$func.searchByLocationId(
-          event.target.value,
-          _vm.$store.state.nowshowing
-        );
-        _vm.upcomingMovies = _vm.$func.searchByLocationId(
-          event.target.value,
-          _vm.$store.state.upcoming
-        );
+      _vm.ddlLanguageVal = event.target.value * 1;
+      _vm.nowShowingMovies = _vm.$store.state.nowshowing;
+      _vm.upcomingMovies = _vm.$store.state.upcoming;
+      _vm.filterByLanguage(
+        _vm,
+        _vm.ddlLanguageVal,
+        _vm.nowShowingMovies,
+        _vm.upcomingMovies
+      );
+    },
+    filterByLocationEvent(event) {
+      const _vm = this;
+      _vm.ddlLocationVal = event.target.value * 1;
+      _vm.nowShowingMovies = _vm.$store.state.nowshowing;
+      _vm.upcomingMovies = _vm.$store.state.upcoming;
+      _vm.filterByLocation(
+        _vm,
+        _vm.ddlLocationVal,
+        _vm.nowShowingMovies,
+        _vm.upcomingMovies
+      );
+    },
+    sortListEvent() {
+      const _vm = this;
+      if (_vm.sort.sortby === "ASC") {
+        _vm.sort.sortby = "DESC";
+        _vm.sort.icon = "desc";
+        _vm.sort.title = "Z to A";
       } else {
-        _vm.nowShowingMovies = _vm.$store.state.nowshowing;
-        _vm.upcomingMovies = _vm.$store.state.upcoming;
+        _vm.sort.sortby = "ASC";
+        _vm.sort.icon = "asc";
+        _vm.sort.title = "A to Z";
       }
+      _vm.sortData();
     }
   }
 };
